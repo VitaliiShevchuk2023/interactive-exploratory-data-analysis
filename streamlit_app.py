6722ade8cd81aadc7df352e125a3740c05a87af3
Vitalii Shevchuk <126573180+VitaliiShevchuk2023@users.noreply.github.com>
2025-03-02 18:50:15 +0000

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,46 @@
+#=======================================================================
+## 0.1 Імпорт бібліотек, дані
 
-# Імпортування необхідних бібліотек
+#Імпортуємо необхідні пакети
 import streamlit as st
 import openpyxl
 import pygwalker as pyg
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime as dt
+import sys
+import os
+from PIL import Image
 
-# Налаштування веб-сторінки
-st.set_page_config(page_title='Додаток для дослідницького аналізу даних', page_icon=None, layout="wide")
+#Отримуємо шлях до зображень
+cwd_dir = os.path.dirname(__file__)
+rel_path = './images'
+images_path = os.path.join(cwd_dir,rel_path)
+logo = Image.open(images_path + '/Browser Icon Reverse.png')
 
+#Налаштування сторінки Streamlit
+st.set_page_config(page_title = 'Додаток для розвідувального аналізу даних',page_icon = logo,layout = "wide")
 
-# Створення розділу в бічній панелі
-st.sidebar.write("****A) Завантаження файлу****")
+#Створюємо контейнер з колонками для заголовка сторінки, конкретного заголовка та логотипу
+with st.container():
+    col1, col2, padding = st.columns([10,1,1])
 
-# Вибір типу файлу
-ft = st.sidebar.selectbox("*Який тип файлу?*", ["Excel", "csv"])
+    #Встановлюємо заголовок та підзаголовок сторінки
+    with col1:
+        st.markdown("### Додаток для розвідувального аналізу даних")
+        
+    #Встановлюємо логотип
+    with col2:
+        niv_logo = Image.open(images_path + '/Social Logo.png')
+        st.image(niv_logo, width=200, output_format='auto')
 
-# Створення динамічного варіанту завантаження
+st.sidebar.write("****А) Завантаження файлу****")
+
+#Запит користувача для вибору типу файлу
+ft = st.sidebar.selectbox("*Який тип файлу?*",["Excel", "csv"])
+
+#Створюємо динамічну опцію завантаження файлу в бічній панелі
 uploaded_file = st.sidebar.file_uploader("*Завантажте файл тут*")
 
 if uploaded_file is not None:
@@ -23,98 +48,134 @@ if uploaded_file is not None:
 
     if ft == 'Excel':
         try:
-            # Вибір аркуша в Excel-файлі
-            sh = st.sidebar.selectbox("*Який аркуш треба прочитати?*", pd.ExcelFile(file_path).sheet_names)
-            # Вибір рядка з назвами стовпців
-            h = st.sidebar.number_input("*У якому рядку містяться назви стовпців?*", 0, 100)
+            #Запит користувача для вибору імені аркуша в завантаженому Excel
+            sh = st.sidebar.selectbox("*Який аркуш у файлі слід прочитати?*",pd.ExcelFile(file_path).sheet_names)
+            #Запит користувача для визначення рядка з назвами стовпців, якщо вони не в рядку заголовка в завантаженому Excel
+            h = st.sidebar.number_input("*У якому рядку містяться назви стовпців?*",0,100)
         except:
-            st.info("Файл не розпізнається як Excel")
+            st.info("Файл не розпізнано як Excel файл")
             sys.exit()
     
     elif ft == 'csv':
         try:
-            # Для csv не потрібні sh та h
+            #Для csv не потрібно sh та h, встановлюємо їх як None
             sh = None
             h = None
         except:
-            st.info("Файл не розпізнається як csv")
+            st.info("Файл не розпізнано як csv файл.")
             sys.exit()
 
-    # Функція кешування для завантаження даних
+    #Функція кешування для завантаження даних
     @st.cache_data(experimental_allow_widgets=True)
-    def load_data(file_path, ft, sh, h):
+    def load_data(file_path,ft,sh,h):
         
         if ft == 'Excel':
             try:
-                # Читання Excel-файлу
-                data = pd.read_excel(file_path, header=h, sheet_name=sh, engine='openpyxl')
+                #Читаємо Excel файл
+                data = pd.read_excel(file_path,header=h,sheet_name=sh,engine='openpyxl')
             except:
-                st.info("Файл не розпізнається як Excel")
+                st.info("Файл не розпізнано як Excel файл.")
                 sys.exit()
     
         elif ft == 'csv':
             try:
-                # Читання csv-файлу
+                #Читаємо csv файл
                 data = pd.read_csv(file_path)
             except:
-                st.info("Файл не розпізнається як csv")
+                st.info("Файл не розпізнано як csv файл.")
                 sys.exit()
         
         return data
 
-    data = load_data(file_path, ft, sh, h)
+    data = load_data(file_path,ft,sh,h)
+#=======================================================================
+## 0.2 Попередня обробка наборів даних
 
+    #Заміна підкреслень у назвах стовпців на пробіли
+    data.columns = data.columns.str.replace('_',' ') 
+
+    data = data.reset_index()
+
+    #Перетворення назв стовпців на заголовний регістр
+    data.columns = data.columns.str.title()
+
+    #Горизонтальний роздільник
+    st.sidebar.divider()
+#=====================================================================================================
 ## 1. Огляд даних
-    st.write('### 1. Попередній перегляд набору даних')
+    st.write( '### 1. Попередній перегляд набору даних ')
 
     try:
-        # Відображення датафрейму в Streamlit
-        st.dataframe(data, use_container_width=True)
+      #Перегляд таблиці даних у streamlit
+      st.dataframe(data, use_container_width=True,hide_index=True)
+
     except:
-        st.info("Файл не було правильно прочитано. Переконайтеся, що вхідні параметри визначено правильно.")
-        sys.exit()
+      st.info("Файл не був правильно прочитаний. Переконайтеся, що вхідні параметри визначені правильно.")
+      sys.exit()
 
+    #Горизонтальний роздільник
+    st.divider()
+#=====================================================================================================
 ## 2. Розуміння даних
-    st.write('### 2. Загальний огляд')
+    st.write( '### 2. Загальний огляд ')
 
-    # Створення радіокнопок у бічній панелі
-    selected = st.sidebar.radio("**B) Що ви хочете дізнатися про дані?**", 
-                               ["Розміри даних",
-                                "Описи полів",
-                                "Статистика", 
-                                "Кількість значень полів"])
+    #Створення радіо-кнопки та бічної панелі одночасно
+    selected = st.sidebar.radio( "**Б) Що ви хочете знати про дані?**", 
+                                ["Розміри даних",
+                                 "Опис полів",
+                                "Статистичні показники", 
+                                "Підрахунок значень полів"])
 
-    # Відображення типів полів
-    if selected == 'Описи полів':
-        fd = data.dtypes.reset_index().rename(columns={'index':'Назва поля', 0:'Тип поля'}).sort_values(by='Тип поля', ascending=False).reset_index(drop=True)
-        st.dataframe(fd, use_container_width=True)
+    #Показ типів полів
+    if selected == 'Опис полів':
+        fd = data.dtypes.reset_index().rename(columns={'index':'Назва поля',0:'Тип поля'}).sort_values(by='Тип поля',ascending=False).reset_index(drop=True)
+        st.dataframe(fd, use_container_width=True,hide_index=True)
 
-    # Відображення статистики
-    elif selected == 'Статистика':
+    #Показ статистичних показників
+    elif selected == 'Статистичні показники':
         ss = pd.DataFrame(data.describe(include='all').round(2).fillna(''))
+        #Додавання кількості нульових значень до статистичних показників
+        nc = pd.DataFrame(data.isnull().sum()).rename(columns={0: 'кількість_null'}).T
+        ss = pd.concat([nc,ss]).copy()
         st.dataframe(ss, use_container_width=True)
 
-    # Відображення кількості значень у полях
-    elif selected == 'Кількість значень полів':
-        # Створення радіокнопки в бічній панелі для цього вибору
-        sub_selected = st.sidebar.radio("*Яке поле потрібно дослідити?*", data.select_dtypes('object').columns)
+    #Показ кількості значень об'єктних полів
+    elif selected == 'Підрахунок значень полів':
+        # створення радіо-кнопки та бічної панелі одночасно, якщо вибрана ця основна опція
+        sub_selected = st.sidebar.radio( "*Яке поле слід дослідити?*",data.select_dtypes('object').columns)
         vc = data[sub_selected].value_counts().reset_index().rename(columns={'count':'Кількість'}).reset_index(drop=True)
-        st.dataframe(vc, use_container_width=True)
+        st.dataframe(vc, use_container_width=True,hide_index=True)
 
-    # Відображення розмірів датафрейму
+    #Показ розмірів таблиці даних
     else:
-        st.write('###### Дані мають розміри:', data.shape)
+        st.write('###### Дані мають розміри:',data.shape)
 
+    #Горизонтальний роздільник
+    st.divider()
 
+    #Горизонтальний роздільник у бічній панелі
+    st.sidebar.divider()
+#=====================================================================================================
 ## 3. Візуалізація
 
-    # Вибір необхідності візуалізації
-    vis_select = st.sidebar.checkbox("**C) Потрібна візуалізація для цього набору даних?**")
+    #Вибір, чи потрібна візуалізація
+    vis_select = st.sidebar.checkbox("**В) Чи потрібна візуалізація для цього набору даних (приховайте бічну панель для повного огляду панелі інструментів)?**")
 
     if vis_select:
-        st.write('### 3. Візуальний аналіз')
 
-        # Створення панелі PyGWalker
-        walker = pyg.walk(data, return_html=True)
-        st.components.v1.html(walker, width=1100, height=800)  # Налаштуйте ширину та висоту за потреби
+        st.write( '### 3. Візуальні дані ')
+
+        try:
+            #Створення панелі PyGWalker та відображення HTML-рядка в Streamlit
+            walker_html = pyg.walk(data).to_html()
+            st.components.v1.html(walker_html, height=1000)
+            
+        except Exception as e:
+            st.error(f"Сталася помилка: {e}")
+            
+else:
+    st.info("Будь ласка, завантажте файл, щоб продовжити.")
+
+#Горизонтальний роздільник
+st.divider()
 
